7e9f7d4a29e121bf42d71bb1283476037a7ce22c
Tania Diachenko <tetianamyhun@gmail.com>
2017-10-30 15:51:00 +0200

    create a project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+	$ ('.menuToggle').on('click', function () {
+		$('.dropdown').slideToggle(200);
+			/*, function() {
+			if($(this).css('display') === 'none') {
+			$(this).removeAttr('style');
+			}*/
+		/*});*/
+	});
+});
+
+
+		
+
+
